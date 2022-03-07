c289ef591936c7b7480352f7e27c5c0b4a313d0f
Oreganon <oreganon@protonmail.com>
2022-03-07 17:25:31 +0100

    new true script

---
+++ imdb/README.md
@@ -0,0 +1,5 @@
+
+
+IMDB data from https://www.kaggle.com/rounakbanik/the-movies-dataset?select=movies_metadata.csv
+
+Licensed under CC0
---
+++ imdb/filter.py
@@ -0,0 +1,17 @@
+import json, os
+
+j = []
+
+with open("movies.json", "r") as f:
+    j = json.loads(f.read())
+
+exists = os.listdir("../screenshots")
+
+filtered = []
+
+for m in j:
+    filtered.append([m[0], m[1], "true" if m[0] in exists else "false"])
+
+
+with open("movies.json", "w") as f:
+    f.write(json.dumps(filtered))
