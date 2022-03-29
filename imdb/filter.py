0b46d615a43095af77ed1ef0685e44763161be54
Oreganon <oreganon@protonmail.com>
2022-03-29 07:27:56 +0000

    use only recentish movies

--- imdb/filter.py
+++ imdb/filter.py
@@ -1,4 +1,4 @@
-import json, os
+import json, os, csv
 
 j = []
 
@@ -9,8 +9,25 @@ exists = os.listdir("../screenshots")
 
 filtered = []
 
+newish = []
+with open('movies_metadata.csv', mode ='r') as f:
+   
+  # reading the CSV file
+  csvFile = csv.DictReader(f)
+ 
+  # displaying the contents of the CSV file
+  for lines in csvFile:
+      if (lines["release_date"] == None or not lines["release_date"].startswith("20")):
+          continue
+      newish.append(lines["imdb_id"])
+
+print(newish)
+
 for m in j:
-    filtered.append([m[0], m[1], "true" if m[0] in exists else "false"])
+    if m[0] not in newish:
+        filtered.append([m[0], m[1], "false"])
+    else:
+        filtered.append([m[0], m[1], "true" if m[0] in exists else "false"])
 
 
 with open("../movies.json", "w") as f:
